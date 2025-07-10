3fb4aace2bc8a6de418221c0e685bad481a1db82
sivarajesh17 <sivarajeshsigireddy7032@gmail.com>
2025-07-10 23:07:27 +0530

    up counter

---
+++ Day40/up_counter_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+module up_counter_tb;
+
+    reg clk, reset;
+    wire [3:0] count;
+
+    up_counter uut (
+        .clk(clk),
+        .reset(reset),
+        .count(count)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        $display("Time\tclk rst | count");
+        $monitor("%0dns\t  %b   %b  |  %b", $time, clk, reset, count);
+
+        reset = 1; #10;
+        reset = 0;
+
+        #100;
+        reset = 1; #10;
+        reset = 0;
+
+        #50;
+        $finish;
+    end
+
+endmodule
+
